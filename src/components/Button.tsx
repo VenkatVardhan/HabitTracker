4c2445040b067de6147a2b495df1eed2eacc8eab
Venkat-Vardhan <saivenkat1579@gmail.com>
2026-06-13 22:02:45 +0530

    added HabbitList

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ type ButtonProps={
 
 export default function Button({children}:ButtonProps){//destructuring props
   return (
-    <button disabled className="bg-violet-600 hover:bg-violet-500 rounded px-3 py-1 disabled:opacity-35 disabled:cursor-not-allowed">
+    <button  className="bg-violet-600 hover:bg-violet-500 rounded px-3 py-1 disabled:opacity-35 disabled:cursor-not-allowed">
       {/* default prop key called children represnsts whateever is between the compoenet */}
       {children}
     </button>
--- src/components/HabbitList.tsx
+++ src/components/HabbitList.tsx
@@ -1,16 +1,59 @@
-export default function HabbitList(){
-  const habbits = ["one","two","three"]
-  if(habbits.length===0){
-    return <h1 className="text-center text-zinc-500 py-12"
-    >No habbits yet. add one to get started</h1>
+import Button from './Button'
+import {
+  eachDayOfInterval,
+  endOfWeek,
+  
+  startOfWeek,
+} from 'date-fns'
+
+export default function HabbitList() {
+  const habbits = [
+    { id: 1, name: 'one' },
+    { id: 2, name: 'two' },
+  ]
+  if (habbits.length === 0) {
+    return (
+      <h1 className='text-center text-zinc-500 py-12'>
+        No habbits yet. add one to get started
+      </h1>
+    )
   }
   return (
-    <div className="flex flex-col gap-3">
-
-      {habbits.map((habbit,i)=>{
-        return <h1 key={i}>{habbit}</h1>
+    <div className='flex flex-col gap-3'>
+      {habbits.map((habbit, i) => {
+        return <HabbitItem key={habbit.id} habbit={habbit} />
       })}
-      
+    </div>
+  )
+}
+type HabbitItemProps = {
+  habbit: { id: number; name: string }
+}
+
+function HabbitItem({ habbit }: HabbitItemProps) {
+  const visibleDates = eachDayOfInterval({
+    start: startOfWeek(new Date()),
+    end: endOfWeek(new Date()),
+  })
+  return (
+    <div className='rounded-xl bg-zinc-800 p-4 flex-col flex gap-3'>
+      <div className='flex ietms-center justify-between '>
+        <div className='flex gap-3'>
+          <span>{habbit.name}</span>
+          <span>🔥 3</span>
+        </div>
+        <Button>Delete</Button>
+      </div>
+      <div className='flex gap-1.5'>
+        {visibleDates.map((date) => (
+          <Button key={date.toISOString()}>
+            <div className='flex flex-col g-3'>
+              <span>Monday</span>
+              <span>{date.getDay()}</span>
+            </div>
+          </Button>
+        ))}
+      </div>
     </div>
   )
 }
